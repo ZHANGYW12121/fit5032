f64c82b02c6f0caa4a9c6f6cc085e866f1ddac70
ZHANGYW12121 <490331729@qq.com>
2025-08-16 17:37:31 +0800

    Complete Vue.js project with all files

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), vueDevTools()],
-  base: '/fit5032/',
+  base: './', // ← 改为相对路径！
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
